f2516696766ee4121e90f0c2d0973d86e08099d5
Tundesamson26 <odewoletundesamson@gmail.com>
2023-08-21 17:30:01 +0100

    updated commit

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full bg-gray-100">
     <div class="flex justify-between items-center h-[50px]">
-        <h1>hello</h1>
+        <h1>hello header</h1>
     </div>
   </div>
 </template>
