f3ebc0744b8cc1e898cc97e7b4c64af5fd6668fc
Tundesamson26 <odewoletundesamson@gmail.com>
2023-08-23 17:37:47 +0100

    updated dashboard commit

--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -4,23 +4,15 @@
       <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
         <div class="grid grid-cols-3 gap-4 mb-4">
           <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-            <p class="text-2xl text-gray-400 dark:text-gray-500">
-              <svg
-                class="w-3.5 h-3.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 18"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 1v16M1 9h16"
-                />
-              </svg>
-            </p>
+            <div class="text-sm text-gray-400 dark:text-gray-500">
+              <!--  -->
+              <div>
+                <div class="bg-green-300 rounded-xl p-1 text-[12px]">
+                  <p>^<span>12%</span></p>
+                </div>
+                <div></div>
+              </div>
+            </div>
           </div>
           <div class="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
             <p class="text-2xl text-gray-400 dark:text-gray-500">
@@ -262,10 +254,8 @@
 
 <script>
 export default {
-    name: "CardBox"
+  name: 'CardBox'
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
